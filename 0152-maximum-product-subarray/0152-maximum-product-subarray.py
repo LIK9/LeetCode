d2c104692f4f02a71b154d845908dd408454b113
LIK9 <107855191+LIK9@users.noreply.github.com>
2024-06-08 20:47:52 +0900

    Time: 73 ms (49.27%), Space: 16.8 MB (99.56%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,22 +1,16 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
 
-        # 최대 곱과 최소 곱을 저장
-        max_so_far = nums[0]
-        min_so_far = nums[0]
-        result = max_so_far
+        currentMax = nums[0]
+        currentMin = nums[0]
+        prodMax = nums[0]
 
         for i in range(1, len(nums)):
-            # 현재 요소가 음수인 경우, 최소 곱을 업데이트해야 함
-            # 왜냐하면 음수와 음수를 곱하면 양수가 될 수 있기 때문
-            curr = nums[i]
-            temp_max = max(curr, max_so_far * curr, min_so_far * curr)
-            min_so_far = min(curr, max_so_far * curr, min_so_far * curr)
+            vals = (nums[i], currentMax*nums[i], currentMin*nums[i])
+            currentMax = max(vals)
+            currentMin = min(vals)
 
-            max_so_far = temp_max
+            prodMax = max(currentMax, prodMax)
 
-            result = max(result, max_so_far)
-
-        return result
+        return prodMax
+        
